0d0bdb01c36854bff0751205c7dfd9ef0fc0ae34
ilyastuit <ilyastuit@gmail.com>
2021-01-08 16:10:27 +0300

    initial files

---
+++ src/test/java/com/ilyastuit/book_library_spring/BookLibrarySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyastuit.book_library_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibrarySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
